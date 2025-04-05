95f4d5cc24778a62b85cf6782f53a89aafefc033
Ashishkol108 <ashishkolteke7@gmail.com>
2025-04-05 12:59:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-26"
-    key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    bucket         = "my-bucket-name"
+    key            = "path/to/my/key"
+    region         = "us-east-1"
+    use_lockfile   = true  # New parameter replacing dynamodb_table
   }
 }
+
